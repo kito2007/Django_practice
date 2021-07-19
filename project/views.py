1cc1730d261907a84c9037e197e931f58b2f50ad
kito2007 <60645183+kito2007@users.noreply.github.com>
2021-07-19 21:32:40 +0900

    html 파일 수정 필요

--- project/views.py
+++ project/views.py
@@ -6,3 +6,8 @@ def project_index(request):
     projects = Project.objects.all().order_by('-created_at')
     context = {'projects':projects}
     return render(request, 'project/project_index.html', context)
+
+def project_detail(request, id):
+    project = Project.objects.get(pk=id)
+    context = {'project':project}
+    return render(request, 'project/project_detail.html', context)
