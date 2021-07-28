de0f310b7f6fbdb259986a8472ddfd70094a8e69
kito2007 <60645183+kito2007@users.noreply.github.com>
2021-07-28 23:46:27 +0900

    이미지 오류 다시 생김

--- project/models.py
+++ project/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Project(models.Model):
     title = models.CharField('타이틀', max_length=200)
     description = models.TextField('프로젝트 내용')
     tech = models.TextField('사용기술', max_length=100)
-    image = models.ImageField('프로젝트 이미지',blank=True, null = True, upload_to='project')
+    image = models.ImageField('프로젝트 이미지', blank=True, null=True, upload_to='project')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
