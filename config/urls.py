a9fabfc067b580f09c4c1fc4ad696de7bfda3ab9
kito2007 <60645183+kito2007@users.noreply.github.com>
2021-08-09 22:26:28 +0900

    image attribute error

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('project/', include('project.urls')),
-    path('', until_done.html)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
